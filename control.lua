a252f814b55f9ef7ae0ad1db77fd6a9777c0c057
mgaydash <mitchell@gaydash.net>
2025-10-26 18:24:20 -0400

    chore: refactor and doc for readability

--- control.lua
+++ control.lua
@@ -1,3 +1,9 @@
+-- Configuration constants
+local TILE_SIZE = 32
+local SCREENSHOT_RESOLUTION = 2000
+local SCREENSHOT_ZOOM = 0.5
+local NEIGHBOR_CHECK_RADIUS = 2
+
 function detect_bounds()
   -- Find the things that define the bounds of a base
   local entities = game.player.surface.find_entities_filtered{name = {
@@ -58,38 +64,41 @@ function detect_bounds()
       y_max = math.floor(y)
     end
   end
-  return y_max, y_min, x_max, x_min
+  return x_min, x_max, y_min, y_max
 end
 
 function is_near_other(entity)
   -- Check if an entity is nearby another entity
+  -- Note: find_entities includes the entity itself, so we check for > 1
   local x = entity.position.x
   local y = entity.position.y
-  local nearby = entity.surface.find_entities({{x-2, y-2}, {x+2, y+2}})
-  if #nearby > 1 then
-    return true
-  else
-    return false
-  end
+  local nearby = entity.surface.find_entities({
+    {x - NEIGHBOR_CHECK_RADIUS, y - NEIGHBOR_CHECK_RADIUS},
+    {x + NEIGHBOR_CHECK_RADIUS, y + NEIGHBOR_CHECK_RADIUS}
+  })
+  -- More than 1 means the entity itself plus at least one neighbor
+  return #nearby > 1
 end
 
 commands.add_command("screenshot_grid", nil, function(command)
   game.print("Taking a grid of screenshots...")
   
-  local y_max, y_min, x_max, x_min = detect_bounds()
-  local tile_size = 32
-  local x_size = 2000
-  local y_size = 2000
-  local zoom = 0.5
+  local x_min, x_max, y_min, y_max = detect_bounds()
+  
+  -- Calculate factory dimensions and center point
+  local factory_width = x_max - x_min
+  local factory_height = y_max - y_min
   local x_center = (x_max + x_min) / 2
   local y_center = (y_max + y_min) / 2
-  local anti_alias = false
-  local ent_info = true
   
-  -- Dividing by 2 because we need a radius and have the whole distance between edges
-  local x_radius = math.ceil((math.abs(x_min) + math.abs(x_max)) * tile_size / x_size / 2 * zoom)
-  local y_radius = math.ceil((math.abs(y_min) + math.abs(y_max)) * tile_size / y_size / 2 * zoom)
-
+  -- Calculate how many tiles wide/tall each screenshot covers in world coordinates
+  local tiles_per_screenshot_x = SCREENSHOT_RESOLUTION / TILE_SIZE / SCREENSHOT_ZOOM
+  local tiles_per_screenshot_y = SCREENSHOT_RESOLUTION / TILE_SIZE / SCREENSHOT_ZOOM
+  
+  -- Calculate grid radius (number of screenshots needed in each direction from center)
+  local x_radius = math.ceil(factory_width / tiles_per_screenshot_x / 2)
+  local y_radius = math.ceil(factory_height / tiles_per_screenshot_y / 2)
+  
   -- Disable extra textures on the surface that will add noise to the map
   game.player.surface.daytime = 0
   game.player.surface.show_clouds = false
@@ -100,22 +109,23 @@ commands.add_command("screenshot_grid", nil, function(command)
   for row_offset = -y_radius, y_radius do
       for col_offset = -x_radius, x_radius do
           -- Calculate world position for this grid cell
-          local screenshot_x = col_offset * x_size / tile_size / zoom + x_center
-          local screenshot_y = row_offset * y_size / tile_size / zoom + y_center
+          local screenshot_x = col_offset * tiles_per_screenshot_x + x_center
+          local screenshot_y = row_offset * tiles_per_screenshot_y + y_center
           
           -- Convert loop indices to 0-based filename indices for stitching
           local filename_row = row_offset + y_radius
           local filename_col = col_offset + x_radius
           
           game.take_screenshot {
-              resolution = {x = x_size, y = y_size},
-              zoom = zoom,
+              resolution = {x = SCREENSHOT_RESOLUTION, y = SCREENSHOT_RESOLUTION},
+              zoom = SCREENSHOT_ZOOM,
               position = {screenshot_x, screenshot_y},
-              show_entity_info = ent_info,
-              anti_alias = anti_alias,
+              show_entity_info = true,
+              anti_alias = false,
               path = "image_" .. filename_row .. "_" .. filename_col .. ".png"
           }
       end
   end
+  
   game.print("Done.")
 end)
